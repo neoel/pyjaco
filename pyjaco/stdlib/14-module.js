ff54d051f5165c7486509450b22990291e6057a3
Dexter <a.esselink@gmail.com>
2012-05-30 09:03:52 +0200

    Added import statements

--- pyjaco/stdlib/14-module.js
+++ pyjaco/stdlib/14-module.js
@@ -26,14 +26,126 @@
 var module = __inherit(object, "module");
 
 module.PY$__init__ = function(modname, filename, objects) {
+    this.PY$__name__ = modname;
     this.modname = modname;
     this.filename = filename;
     if (objects !== undefined) {
         for (var o in objects) {
-            if (o.charAt(2) === "$")
+            if (o.charAt(2) === "$") {
                 this[o] = objects[o];
-        };
+            }
+        }
     }
+};
+
+module.__import = function (name, asname) {
+    
+    // load the object
+    var mod = this.__load(name);
+    
+    // insert the imported module in the current module.
+    this["PY$"+(asname||name)] = mod;
+    
+};
+module.__import_from = function (from, name, asname) {
+    var modname, loaded_module;
+    // the import statement first check's the current module. after that, it checks the global space
+//     console.log("from", from, 'import', name);
+    
+    modname = from + '.' + name;
+    
+    // import from first
+    from = this.__load(from)
+    
+    loaded_module = __builtins__.PY$getattr(from, name, false);
+        
+    if (!loaded_module) {
+        loaded_module = this.__load(modname);
+    }
+    if (!loaded_module) {
+        throw __builtins__.PY$ImportError("ImportError: Cannot import name "+name);
+    }
+    
+    this["PY$"+(asname||name)] = loaded_module;
+};
+module.__load = function(name) {
+    /**
+     * This function loads module name and loads each parentmodule first in order.
+     */
+    
+    var i, mod,nextmod, splitted;
+    
+    splitted = name.split('.');
+    
+    for (i=1; i<=splitted.length; i++) {
+        if (!mod) {
+            mod = this.__load_module(splitted.slice(0, i).join('.'));
+        } else {
+            nextmod = mod.__load_module(splitted.slice(0, i).join('.'));
+            if (!__builtins__.PY$getattr(mod, splitted[i-1], false)) { 
+                mod["PY$"+splitted[i-1]] = nextmod;
+            }
+            mod = nextmod;  
+        }
+    }
+    return mod;
+    
+};
+module.__load_module = function(modname) {
+    
+    /**
+     * Tries to load modname, first from loaded_modules
+     */
+    var mod, loaded_module;
+    /*
+    # for each name
+    #   = it inside the current module? ( a preloaded variable)
+    #   = it part of the current relative path?
+    #   = it loaded in loaded_modules
+    #   = it available in available_modules
+    #   -> Try to call lazy_import
+    #   -> Raise error 
+    */
+    
+    if ($PY.loaded_modules.hasOwnProperty(modname)) {
+        // search the loaded_modules for the modname
+        return $PY.loaded_modules[modname];
+    } else if ($PY.available_modules.hasOwnProperty(modname)) {
+        // search the available modules for the modname
+        mod = $PY.available_modules[modname];
+    } else {
+        if ($PY.load_lazy) {
+            // call the lazy loader
+            $PY.load_lazy(modname); // loads the module from the internet, it must be available after it's been called
+            
+            if ($PY.available_modules.hasOwnProperty(modname)) {
+                mod = $PY.available_modules[modname];
+            }
+        }
+    }
+    
+    // raise error
+    if (!mod) {
+        return false;
+        throw __builtins__.PY$ImportError("ImportError: No module named "+modname);
+    }
+
+    if (!!mod) {
+        // create the module
+        loaded_module = module(mod.name, mod.filename);
+        loaded_module.modname = modname;
+        
+        // insert the module object into loaded_modules.
+        $PY.loaded_modules[modname] = loaded_module;
+        
+        // call the module's body
+        mod.body(loaded_module);
+    }
+    // insert the module in the current.
+//     this["PY$"+name] = loaded_module;
+    
+    return loaded_module;
+    
 };
 
 module.PY$__getattr__ = function(k) {
