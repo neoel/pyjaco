7d7d663089fff55fe7bebdf705a280b22a3a5758
Dexter <dexter@dxtr.be>
2012-12-12 17:17:31 +0100

    Added an autoreload to the start script (does require tornado)

--- pyjaco.new/start.py
+++ pyjaco.new/start.py
@@ -1,16 +1,22 @@
-
+import os
+os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
 
 from ist.reader import Reader
 from ist.writer import PythonWriter, JavascriptWriter
 from ist.transform import __ifyTransformer, TypifyTransformer, SemantifyTransformer
 
-test = "getattributes"
+import tornado.autoreload
+
+test = "functions"
 
 print "\n\n########## Translating tests/{}.py:".format(test)
 
 with open("output.js", "w") as output:
     # source = open("base_test.py").read()
-    source = open("tests/{}.py".format(test)).read()
+    location = "tests/{}.py".format(test)
+    source = open(location).read()
+
+    tornado.autoreload.watch(location)
 
     reader = Reader()
     reader.read('__main__', source)
@@ -20,14 +26,37 @@ with open("output.js", "w") as output:
     transformed = SemantifyTransformer(transformed).transform()
 
     python = PythonWriter(reader.collection)
-    python2 = PythonWriter(transformed)
     javascript = JavascriptWriter(transformed)
     
     print "\n########## python code:"
     print python.get('__main__')
-    print "\n########## transformed python code:"
-    print python2.get('__main__')
     print "\n########## transformed javascript code:"
     print javascript.get('__main__')
 
     output.write(javascript.get('__main__'))
+
+
+# import tornado.ioloop
+# import tornado.web
+
+
+# class MainHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         self.write(javascript.get('__main__'))
+
+
+# if __name__ == "__main__":
+#     settings = {
+#         "debug"         : True
+#     }
+
+#     application = tornado.web.Application([
+#         (r"/", MainHandler),
+#     ], **settings)
+
+#     application.listen(8002, 'localhost')
+#     print "Starting server"
+#     tornado.ioloop.IOLoop.instance().start()
+
+
+
